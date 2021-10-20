55ae916ef7813f0285c352765873af01ab3d1eeb
aborisenko <borisenko.anatoly@gmail.com>
2021-10-21 01:01:54 +0300

    Helper object

---
+++ src/main/scala/Helper.scala
@@ -0,0 +1,18 @@
+import java.io.File
+
+object Helper {
+
+  def convertToFileURL(filename:String):String = {
+    var path = new File(filename).getAbsolutePath()
+    if (File.separatorChar != '/') {
+      path = path.replace(File.separatorChar, '/');
+    }
+
+    //    if (!path.startsWith("/")) {
+    //      path = "/" + path;
+    //    }
+
+    path
+  }
+
+}
