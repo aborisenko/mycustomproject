04b69668790b63d8fb10101f7784f8ff111b6320
aborisenko <borisenko.anatoly@gmail.com>
2021-11-15 22:04:43 +0300

    описание --help bugfix для xlsxOpener.getNames вывод исключений в консоль параметр --page для указания страницы (но это видимо временно)

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,21 +1,31 @@
-import Helper.convertToFileURL
 import SAXParser.Handlers.SberDealsXLSXParserHandler._
 import models.Deal
+import Helper._
 
-import java.io.{BufferedOutputStream, FileNotFoundException, FileOutputStream, PrintStream}
-import java.lang.System.console
+import java.io.{BufferedOutputStream, File, FileNotFoundException, FileOutputStream, PrintStream}
+import scala.util.control.NonFatal
 import scala.util.{Failure, Try, Using}
 
 case class UnknownCommandException( message: String) extends Exception
 case class UnknownException(message: String) extends Exception
 case class PrintHelp( message: String =
-                      s"""blablabla
+                      s"""--cmd - echo, echopage or sber_deals
+                         |--input - path to xlsx input file
+                         |--output - path to output file or it's going to write results to the system console
+                         |--page - in case you want to specify the page ot parse, using 'xl/worksheets/sheet1.xml' by default
+                         |--help - to print this sheet
                          |""".stripMargin) extends Exception
 
-case class Task( command: Option[String], file: Option[String], output: Option[String])
+case class Task( command: Option[String] = None, file: Option[String] = None, output: Option[String] = None, page: Option[String] = None){
 
-object Task{
-  def apply(): Task = Task(None, None, None)
+  override def toString: String = {
+    s"""Task(
+       |\tcommand     = $ANSI_GREEN ${command.getOrElse("")} $ANSI_RESET
+       |\tinput file  = $ANSI_GREEN ${file.getOrElse("")} $ANSI_RESET
+       |\toutput file = $ANSI_GREEN ${output.getOrElse("")} $ANSI_RESET
+       |\tpage file   = $ANSI_GREEN ${page.getOrElse("")} $ANSI_RESET
+       |""".stripMargin
+  }
 }
 
 object App {
@@ -30,8 +40,9 @@ object App {
     taskE flatMap { task =>
       arg match {
         case (Some("--cmd"), opt1@Some(_)) => Right(task.copy(command = opt1))
-        case (Some("--file"), opt2:Some[String]) => Right(task.copy(file = opt2))
-        case (Some("--output"), opt3@Some(_)) => Right(task.copy(output = opt3))
+        case (Some("--input"), opt2:Some[String]) => Right(task.copy(file = opt2))
+        case (Some("--output"), opt3:Some[String]) => Right(task.copy(output = opt3))
+        case (Some("--page"), opt4:Some[String]) => Right(task.copy(page = opt4))
         case (Some("--help"), _) => Left( PrintHelp())
         case _ => Left( PrintHelp())
       }
@@ -43,27 +54,32 @@ object App {
     val taskE: Either[Exception, Task] =
       args.map(splitArgsIntoTuple).foldLeft[Either[Exception, Task]](Right(Task()))(argsIntoTask)
 
+    println(taskE.getOrElse(""))
+
     for {
       task <- taskE
       filename <- task.file.toRight(new FileNotFoundException("filename must be specified"))
+      inputF = new File(filename)
     } yield Using(
-
       task.output match {
         case Some(filename) => new PrintStream(new BufferedOutputStream(new FileOutputStream(filename)))
         case None => System.out
-
-      }) { output: PrintStream =>
+      }
+    ){ output: PrintStream =>
 
       task.command match {
-        case Some("echo") => xlsxOpener.getNames(filename) {
-          output.println
-        }
-        case Some("echopage") => xlsxOpener.openOne(filename, dataXmlFileName) {
-          xmlFile => ()
-        }
-        case Some("sber_deals") => xlsxOpener.openOne(filename, dataXmlFileName) {
-          xmlFile =>
+        case Some("echo") => xlsxOpener.getNames(inputF)( output.println )
+        case Some("echopage") => xlsxOpener.openOne(filename, task.page.getOrElse(dataXmlFileName)) { xmlFile =>
 
+            val b = new Array[Byte](1024)
+            Iterator.continually{
+              val len = xmlFile.read(b)
+              output.write(b, 0, len)
+              len
+            }.takeWhile(_ > 0).foreach(_ => ())
+
+        }
+        case Some("sber_deals") => xlsxOpener.openOne(filename, task.page.getOrElse(dataXmlFileName)) { xmlFile =>
 
             val dealsHandler = new DealsHandler {
               def addOne(deal: Deal): Unit = output.println(deal.toString)
@@ -75,6 +91,10 @@ object App {
         case _ => Failure( UnknownCommandException(task.command.getOrElse("command must be specified, try --help instead")))
       }
 
+    }.recoverWith {
+      case NonFatal(e) =>
+        System.err.println(e)
+        Failure(e)
     }.flatten.toEither
 
   }
--- src/main/scala/xlsxOpener.scala
+++ src/main/scala/xlsxOpener.scala
@@ -1,28 +1,37 @@
 
 import java.io.{BufferedInputStream, File, FileInputStream, FileNotFoundException, IOException, InputStream}
 import java.util.zip.{ZipFile, ZipInputStream}
-import scala.util.{Try, Using}
+import scala.util.{Try, Using, Failure}
+import scala.util.control.NonFatal
 
 object xlsxOpener {
 
-  def getNames( filename: String)(f: String => Unit ): Try[Unit] = {
-    Using(new ZipInputStream( new BufferedInputStream( new FileInputStream(filename)))) { zin =>
+  def getNames( file: File)(f: String => Unit ): Try[Unit] = {
+    Using(new ZipInputStream( new BufferedInputStream( new FileInputStream(file)))) { zin =>
       Iterator.continually({
         zin.getNextEntry
-      }).takeWhile( _ != null).map( zipEntry => f( zipEntry.getName) )
+      }).takeWhile( _ != null).foreach( zipEntry => f( zipEntry.getName) )
+    }.recoverWith {
+      case NonFatal(e) =>
+        System.err.println(e.getMessage)
+        Failure(e)
     }
   }
 
-  def openAll( filename: String, filterByName: (String) => Boolean)(f: (InputStream) => Unit ): Try[Unit] = {
-
-    Using(new ZipInputStream(new FileInputStream(filename))) { zin =>
-      Iterator.continually({
-        zin.getNextEntry
-      }).takeWhile(_ != null).filter(e => filterByName(e.getName)).tapEach { e =>
-        f(zin)
-      }.to(Iterable)
-    }
-  }
+//  def openAll( filename: String, filterByName: (String) => Boolean)(f: (InputStream) => Unit ): Try[Unit] = {
+//
+//    Using(new ZipInputStream(new FileInputStream(filename))) { zin =>
+//      Iterator.continually({
+//        zin.getNextEntry
+//      }).takeWhile(_ != null).filter(e => filterByName(e.getName)).tapEach { e =>
+//        f(zin)
+//      }.foreach( _ => ())
+//    }.recoverWith {
+//      case e: Exception =>
+//        System.err.println(e.getMessage)
+//        Failure(e)
+//    }
+//  }
 
   //  @throws[FileNotFoundException]
   //  @throws[IOException]
@@ -32,6 +41,10 @@ object xlsxOpener {
       zin =>
         val entry = zin.getEntry(nameToOpen)
         f(zin.getInputStream(entry))
+    }.recoverWith {
+      case NonFatal(e) =>
+        System.err.println(e)
+        Failure(e)
     }
   }
 
